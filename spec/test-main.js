af31c009b87a30dff8e3b1389781917ab5198ae4
Karthik Sirasanagandla <sirkarthik@gmail.com>
2014-01-01 19:49:43 +0530

    Reference Project commit for using RequireJS with Jasmine

---
+++ spec/test-main.js
@@ -0,0 +1,42 @@
+require.config({
+    baseUrl: '',
+    paths: {
+        'domReady': 'lib/domReady', //domReady is a RequireJS plugin
+        'jasmine': 'lib/jasmine-1.1.0/jasmine',
+        'jasmine-html': 'lib/jasmine-1.1.0/jasmine-html',
+
+        'hello': 'src/hello',
+        'helloSpec': 'spec/helloSpec'
+    },
+
+    //You'd shim the 3rd party libs that are not AMD compatible
+    shim: {
+        'jasmine': {
+            exports: 'jasmine'
+        },
+        'jasmine-html': ['jasmine']
+    },
+
+    waitSeconds: 15
+});
+
+require(['domReady', 'jasmine', 'jasmine-html', 'helloSpec'], function(domReady, jasmine) {
+//    console.log("All is well..");
+//    console.log(jasmine);
+// Moved the script from Jasmine's SpecRunner.html scriptlet to this block..
+    var jasmineEnv = jasmine.getEnv();
+    jasmineEnv.updateInterval = 1000;
+
+    var trivialReporter = new jasmine.TrivialReporter();
+
+    jasmineEnv.addReporter(trivialReporter);
+
+    jasmineEnv.specFilter = function(spec) {
+        return trivialReporter.specFilter(spec);
+    };
+
+
+    domReady(function () {
+        jasmineEnv.execute();
+    });
+});
